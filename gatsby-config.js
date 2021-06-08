50bda78c63e0371e50c20f06777efa1d4c2da506
Marcos <marcob@avetti.ca>
2021-06-08 14:14:16 -0400

    Update gatsby-config.js
    updated gatsby config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-strapi',
       options: {
-        apiURL: process.env.DEPLOY_URL ? 'https://dashboard.heroku.com/apps/infinite-sierra-55283/' : 'http://localhost:1337',
+        apiURL: process.env.DEPLOY_URL ? 'https://infinite-sierra-55283.herokuapp.com/' : 'http://localhost:1337',
         collectionTypes: [ // List of the Content Types you want to be able to request from Gatsby.
           'articles',
           'users'
